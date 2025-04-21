e384cc7700a3b27e6e69d75696b805b71cbe1917
Eglė <eglsidlauskaite@gmail.com>
2025-04-21 15:17:39 -0500

    Update download-input.sh

--- bin/download-input.sh
+++ bin/download-input.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10gb
+#SBATCH --job-name=download-process
+#SBATCH --chdir=/work/brahmalab/eglesidl/BMI8540-project
+#SBATCH --error=/work/brahmalab/eglesidl/BMI8540-project/outputs/pre-process.err
+#SBATCH --output=/work/brahmalab/eglesidl/BMI8540-project/outputs/pre-process_log.out
 
-# This script is used to download files of interest from GSE224292.
-# The input files are bigWig files generated from RNA Polymerase II 
-# Serine 5 Phosphorylation CUTAC-seq experiment on mouse embryonic
-# stem cells. 
+# This script is used to download and pre-process the files of interest.
 
-# The data shows open chromatin that is associated with RNA Pol II
-# S5P binding. The data is from two types of cells: 
-# - 2i (cells in a more naïve pluripotent state);
-# - SL (a mix of cells in pluripotent and differentiating state).
+# These input files are sequencing data from CUTAC-seq experiment targeting
+# RNA Polymerase II Serine 5 Phosphorylation on mouse embryonic stem cells.
+# The cells are from SL cells, meaning that a mix of cells in pluripotent
+# and differentiating state can be found in each sample.
+# SRR23310241 - 8 hour Flavopiridol (RNA Pol II inhibitor) treatment.
+# SRR23310243 - no treatment.
+
+# Loading SRA-toolkit to use for data download
+module load SRAtoolkit/2.11
 
 # Define directory to store files:
-input_dir="./inputs"
-cd ${input_dir}
+fasta_input_dir="./inputs/fasta"
+mkdir ${fasta_input_dir}
 
 # Download input files
-echo "Downloading the sample file..."
-wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM7019nnn/GSM7019043/suppl/GSM7019043%5FRNAPII%2DS5P%5FCUTAC%5F2i.mm10.bw
-wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM7019nnn/GSM7019044/suppl/GSM7019044%5FRNAPII%2DS5P%5FCUTAC%5FSL.mm10.bw
+echo "Downloading sample files..."
+prefetch SRR23310241 -O ${fasta_input_dir}
+fasterq-dump --split-files SRR23310241 -O ${fasta_input_dir} --threads 4
+prefetch SRR23310243 -O ${fasta_input_dir}
+fasterq-dump --split-files SRR23310243 -O ${fasta_input_dir} --threads 4
+#wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM7019nnn/GSM7019043/suppl/GSM7019043%5FRNAPII%2DS5P%5FCUTAC%5F2i.mm10.bw
+#wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM7019nnn/GSM7019044/suppl/GSM7019044%5FRNAPII%2DS5P%5FCUTAC%5FSL.mm10.bw
 echo "Done."
